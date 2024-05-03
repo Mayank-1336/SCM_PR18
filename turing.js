09bfb1b417f7c533eace2c4e1fd1160c5142f655
@manpreetkaur <manpreet2144.be23@chitkara.edu.in>
2024-05-03 09:32:56 +0530

    d

--- turing.js
+++ turing.js
@@ -2,8 +2,8 @@
 var image =document.getElementsByClassName(".img1");
       
     image.addEventListener("mouseover",function(){
-        image.style.transform = 'scale(1.2)';
-        image.style.transition = 'transform 0.3s ease-in-out';
+        image.style.transform = 'scale(1.1)';
+        image.style.transition = 'transform 1s ease-in-out';
      });
      image.addEventListener("mouseout",function(){
         image.style.transform = 'scale(1)';
